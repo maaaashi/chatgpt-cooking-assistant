3210fc433e900a12d3d226dafb9b0bd8041472e3
maaaashi <doyaseizin.7@gmail.com>
2023-09-15 08:52:14 +0900

    fix compile error

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -17,7 +17,32 @@ export class MaaaashiCookingAssistant extends Stack {
     const prismaLayer = new LayerVersion(this, 'PrismaLayer', {
       compatibleRuntimes: [Runtime.NODEJS_18_X],
       description: 'Prisma Layer',
-      code: Code.fromAsset(path.join(__dirname, '../layer/prisma')),
+      code: Code.fromAsset(path.join(__dirname, '../layer/prisma'), {
+        bundling: {
+          image: Runtime.NODEJS_18_X.bundlingImage,
+          command: [
+            'bash',
+            '-c',
+            [
+              'cp package.json package-lock.json client.js /asset-output',
+              'cp -r prisma /asset-output/prisma',
+              'cp -r node_modules /asset-output/node_modules',
+              'rm -rf /asset-output/node_modules/.cache',
+              'rm -rf /asset-output/node_modules/.prisma/client/*darwin*' ||
+                true,
+              'rm -rf /asset-output/node_modules/.prisma/client/*windows*' ||
+                true,
+              'rm -rf /asset-output/node_modules/@prisma/engines/node_modules',
+              'rm -r /asset-output/node_modules/@prisma/engines/*darwin* || true',
+              'rm -r /asset-output/node_modules/@prisma/engines/*debian* || true',
+              'rm -f /asset-output/node_modules/prisma/*darwin* || true',
+              'rm -f /asset-output/node_modules/prisma/*debian* || true',
+              'rm -f /asset-output/node_modules/prisma/*windows* || true',
+              'npx prisma generate',
+            ].join(' && '),
+          ],
+        },
+      }),
       layerVersionName: 'prisma-layer',
     })
 
