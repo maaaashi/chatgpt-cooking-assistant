fafb6e826d3e3e8e3d18506b0f777aebcc1b1531
maaaashi <doyaseizin.7@gmail.com>
2023-09-15 08:25:56 +0900

    fix compile error

--- lamba-dependency.sh
+++ lamba-dependency.sh
@@ -1,7 +1,7 @@
 for dir in aws/layer/*; do
-  (cd "$dir/nodejs" && npm ci && tsc ./*.ts)
+  (cd "$dir/nodejs" && npm ci && tsc ./*.ts -p ../../../tsconfig.json)
 done
 
 for dir in aws/lambda/*; do
-  (cd "$dir" && npm ci && npx tsc ./index.ts)
+  (cd "$dir" && npm ci && npx tsc ./index.ts -p ../../tsconfig.json)
 done
