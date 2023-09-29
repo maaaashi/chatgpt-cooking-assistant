a55fb01cf534febe31ee0a85dfb93aa4abf7054a
maaaashi <doyaseizin.7@gmail.com>
2023-09-29 11:02:17 +0900

    add api lambda

--- aws/lambda/list-recipes-api/index.ts
+++ aws/lambda/list-recipes-api/index.ts
@@ -19,6 +19,11 @@ export const handler: APIGatewayProxyHandler = async (req) => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'OPTIONS,GET',
+    },
     body,
   }
 }
