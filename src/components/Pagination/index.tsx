cf6e53e6f39392fd768f52de3cd1927406bcdde2
maaaashi <doyaseizin.7@gmail.com>
2023-10-02 08:02:43 +0900

    paginationデザイン調整

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -7,33 +7,64 @@ interface Props {
 }
 
 export const Pagination: FC<Props> = ({ array, clickFunc, now }) => {
-  const mobilePagination = () => {
-    const last = Math.floor(array.length / 10 + 1)
-    let before_2 = now - 2 < 1 ? 0 : now - 2
-    let before_1 = now - 1 < 1 ? 0 : now - 1
-    let after_1 = last < now + 1 ? 0 : now + 1
-    let after_2 = last < now + 2 ? 0 : now + 2
+  const first = 1
+  const last = Math.floor(array.length / 10 + 1)
+  let before_2 = now - 2 < 1 ? 0 : now - 2
+  let before_1 = now - 1 < 1 ? 0 : now - 1
+  let after_1 = last < now + 1 ? 0 : now + 1
+  let after_2 = last < now + 2 ? 0 : now + 2
 
-    const pagination = [before_2, before_1, now, after_1, after_2]
+  let pagination: (string | number)[] = [
+    before_2,
+    before_1,
+    now,
+    after_1,
+    after_2,
+  ].filter((c) => c !== 0)
 
-    return (
-      <div className='join md:hidden'>
-        {pagination.map((p, i) => {
-          if (p === 0) return <div key={i}></div>
+  if (pagination.findIndex((c) => c === first) === -1) {
+    pagination = [1, '・・・', ...pagination]
+  }
 
+  if (pagination.findIndex((c) => c === last) === -1) {
+    pagination = [...pagination, '・・・', last]
+  }
+
+  return (
+    <div className='join'>
+      {pagination.map((p, i) => {
+        if (p === '・・・')
+          return (
+            <button className='join-item btn pointer-events-none' key={i}>
+              {p}
+            </button>
+          )
+
+        if (p === before_2 || p === after_2) {
           return (
             <button
-              className={`join-item btn ${p === now ? 'btn-active' : ''}`}
+              className='join-item btn hidden md:block'
               key={i}
               onClick={() => clickFunc(p)}
             >
               {p}
             </button>
           )
-        })}
-      </div>
-    )
-  }
+        }
+
+        return (
+          <button
+            className={`join-item btn ${p === now ? 'btn-active' : ''}`}
+            key={i}
+            onClick={() => clickFunc(p)}
+          >
+            {p}
+          </button>
+        )
+      })}
+    </div>
+  )
+  const mobilePagination = () => {}
 
   return (
     <>
